18e30a648ace6680a00432da49f94eae2939cac9
pielegacy <alex_billson@outlook.com>
2016-02-21 10:44:02 +1100

    First Commit:

---
+++ Character.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace EvenFunnerGame
+{
+    class Character
+    {
+        public Sprite sprite;
+        public int stat_health = 10;
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+#Jarrod RolePlaying Game
